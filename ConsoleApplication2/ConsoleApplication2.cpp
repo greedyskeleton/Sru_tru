a8110a83a677c4b861208695adff46ac5e4e16a5
krzysztof.kowalik2 <krzysztof.kowalik2@pollub.edu.pl>
2017-11-02 21:20:46 +0100

    OK

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+// ConsoleApplication2.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
+int nope()
+{
+	int a = 1;
+	if a > 0
+		return 1;
+}
